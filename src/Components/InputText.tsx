3cf5fd2a716204b489f78520165d0557f8835408
Robin <robin.somlette@gmail.com>
2018-09-25 16:47:31 -0400

    working components

--- src/Components/InputText.tsx
+++ src/Components/InputText.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 
-export class InputText extends React.Component<{}, {}> {
-  public render() {
-    return <div>Input Text</div>
-  }
+interface IProps {
+	label: string;
+	name: string;
+	value?: string;
+	onChange: (value: string, name: string) => void;
+}
+
+export class InputText extends React.Component<IProps, {}> {
+	public handleOnChange = (event: any) => {
+		const { onChange, name } = this.props;
+
+		onChange(event.target.value, name);
+	};
+
+	public render() {
+		const { label, value, name } = this.props;
+		return (
+			<label htmlFor={name}>
+				<span>{label}</span>
+				<input type="text" name={name} value={value} onChange={this.handleOnChange} />
+			</label>
+		);
+	}
 }
--- src/Containers/App/App.tsx
+++ src/Containers/App/App.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
 import { InputText } from '../../Components/InputText';
 
+interface IState {
+	value: string;
+}
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div>
-        hello world
-        <InputText />
-      </div>
-    );
-  }
+class App extends React.Component<{}, IState> {
+	public state = {
+		value: '',
+	};
+
+	public onChange = (value: string) => {
+		this.setState({ value });
+	};
+
+	public render() {
+		const { value } = this.state;
+		return (
+			<div>
+				<div>hello world</div>
+				<InputText name="input" label="label" value={value} onChange={this.onChange} />
+			</div>
+		);
+	}
 }
 
 export default App;
