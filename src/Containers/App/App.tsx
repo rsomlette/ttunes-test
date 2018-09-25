c6777103da382b9c7b65ca269e250ad658220669
Robin <robin.somlette@gmail.com>
2018-09-25 11:11:18 -0400

    new structure

---
+++ src/Containers/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
